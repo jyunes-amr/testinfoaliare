c1ce5c4a1794ec226b49a1054849b3e04b183d6e
jyunes-amr <jyunes@amr.org.ar>
2026-02-07 19:03:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ==================== CONFIGURACIÓN ====================
 // IMPORTANTE: Reemplazar con la URL de tu Google Apps Script Web App
-const API_URL = 'https://script.google.com/macros/s/AKfycbztUZcWQrSD2O-NFaml62PFqA-B6Gxa0_6YYh7_qgkD5ePhIplQYwjQI89NpdrgMPn9ig/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwEfjoN20MHTB0Nh22g42jNqPsDAs2TKCowogbMnc4W1268OHRLYWsF1t33OMmxzTGynw/exec';
 
 // ==================== ESTADO DE LA APLICACIÓN ====================
 let articles = [];
